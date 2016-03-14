9562aa24fcec4d8d3f27e9cd8dac244b2ef22166
Jack <CptCup@users.noreply.github.com>
2016-03-14 16:08:49 -0400

    Added files via upload
    batMob

---
+++ CAPStone1/Assets/Entities/Mob/BatMob.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatMob : Entities{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.K) || Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.down * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.L) || Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+    }
+}
